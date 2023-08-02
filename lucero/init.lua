07346723306d0b1d61660fd26355260a2b0bb1f2
mnapo <napolimatiase@gmail.com>
2023-08-01 21:13:43 -0300

    Defined initial settings to correctly load the module

--- lucero/init.lua
+++ lucero/init.lua
@@ -1,1 +1,24 @@
---library setup
+--global configuration
+_REQUIRED_CLASSES = {} --determines which classess are necessary
+_DEBUG_MODE_ON = false --set to true to print command-line logs
+
+--path setup
+package.path = package.path..";./?.lua"
+
+--base class loading
+ClassPrototype = require("ClassPrototype")
+
+--necessary classes loading
+
+--debugging prints
+if _DEBUG_MODE_ON then
+    local tests = {
+        [1] = {"package.path", package.path},
+        [2] = {"ClassPrototype", ClassPrototype:debugging_log()}
+    }
+    for i = 1, #tests do
+        print(tests[i][1].." set to "..tests[i][2].." ("..os.time()..")")
+    end
+end
+
+

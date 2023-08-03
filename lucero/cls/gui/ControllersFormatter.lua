a940c8b3d6f752f4a79afc666f9a3e7183e57c6d
mnapo <napolimatiase@gmail.com>
2023-08-02 21:37:28 -0300

    Abstracted class, and added missing end of function and proper returning

--- lucero/cls/gui/ControllersFormatter.lua
+++ lucero/cls/gui/ControllersFormatter.lua
@@ -4,10 +4,6 @@ Formatter = {}
 Formatter.__index = Formatter
 
 function Formatter:new(ControllersList, ListParent)
-    local o = ClassPrototype:new(ControllersList, ListParent)
-    o:set("controllers", ControllersList)
-    o:set("parent", ListParent)
-    return o
 end
 
 function Formatter:format()
